689a3a406db911fc4fc67a5d53755eeaf8559688
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2023-01-25 12:57:02 +1300

    updated

--- frontend/src/components/LoginEvent.jsx
+++ frontend/src/components/LoginEvent.jsx
@@ -1,11 +1,5 @@
-// import React, { useState, useEffect} from "react";
-
-// function UserLogin() {
-//     const [Username,setUsername]=useState("")
-//     const [Password,setPassword]=useState("")
-// }
-
-export default function handleLogin(event) {
+export default function LoginSetup() {
+    function handleLogin(event) {
         event.preventDefault();
     
         var username = document.getElementById("username").value;
@@ -41,3 +35,12 @@ export default function handleLogin(event) {
                 alert("An error occurred. Please try again later.");
             });
     };
+
+    return (
+        <form onsubmit={handleLogin}>
+            <input type='text' id='email' placeholder='email' required='required' />
+            <input type='password' id='password' placeholder='Password' required='required' />
+            <button type='submit' class='btn btn-primary btn-block btn-large'>Let me in.</button>
+        </form>
+    )
+}
