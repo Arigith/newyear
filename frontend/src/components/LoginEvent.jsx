1857fc8726f9aa6f293131c91a8cf98e87be8fe0
Stacey Turner <112356048+Arigith@users.noreply.github.com>
2023-01-25 13:01:39 +1300

    updated

--- frontend/src/components/LoginEvent.jsx
+++ frontend/src/components/LoginEvent.jsx
@@ -2,37 +2,37 @@ export default function LoginSetup() {
     function handleLogin(event) {
         event.preventDefault();
     
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
     
-        fetch("/login", {
-            method: "POST",
+        fetch('/login', {
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json"
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify({ username: username, password: password })
         })
             .then(response => {
                 if (!response.ok) {
-                    throw new Error("HTTP error " + response.status);
+                    throw new Error('HTTP error ' + response.status);
                 }
                 return response.json();
             })
             .then(data => {
                 if (data.access_token) {
                     // Store the access token in the browser's local storage or in a cookie
-                    localStorage.setItem("access_token", data.access_token);
+                    localStorage.setItem('access_token', data.access_token);
                     // Redirect the user to a protected page
-                    window.location.href = "/worklog";
+                    window.location.href = '/worklog';
                 } else {
                     // Handle the error
-                    alert("Invalid username or password");
+                    alert('Invalid username or password');
                 }
             })
             .catch(error => {
                 // Handle the error
-                console.error("Error:", error);
-                alert("An error occurred. Please try again later.");
+                console.error('Error:', error);
+                alert('An error occurred. Please try again later.');
             });
     };
 
